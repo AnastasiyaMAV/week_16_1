50c694a2478046c8199ac34a66bdd0c38f235b11
AnastasiyaMAV <mihalukova112@mail.ru>
2021-09-30 15:26:38 +0200

    Правки после проверки

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 function sumInput(){
-    let mass = [];
-    let lengthMass = prompt('Какой длины будем создавать массив? Введите число', '');
+    let massStr = '';
+    //let lengthMass = prompt('Какой длины будем создавать массив? Введите число', '');
     let summMass = 0;
-    //console.log(typeof(lengthMass));
-    while(lengthMass == ' ' || lengthMass == null || lengthMass == '' || isNaN(lengthMass) == true){
-        alert('Вы ничего не ввели или ввели не число, введите число');
-        lengthMass = prompt('Какой длины будем создавать массив?', '');
-    }
     let enteredNumber;
+    //console.log(typeof(lengthMass));
+    //while(lengthMass == ' ' || lengthMass == null || lengthMass == '' || isNaN(lengthMass) == true){
+    //    alert('Вы ничего не ввели или ввели не число, введите число');
+    //    lengthMass = prompt('Какой длины будем создавать массив?', '');
+    //}
     
-    for(let i = 0; i < lengthMass; i++){
+    
+    // for(let i = 0; i < lengthMass; i++){
+    //     enteredNumber = prompt('Введите целое число!', '');
+    //     console.log(enteredNumber);
+    //     while(enteredNumber == ' ' || enteredNumber == null || enteredNumber == '' || isNaN(enteredNumber) == true){
+    //         alert('Вы ничего не ввели или ввели не число, введите число');
+    //         enteredNumber = prompt('Введите целое число!', '');
+    //     }
+    //     mass[i] = enteredNumber;
+    //     console.log(mass[i]);
+    //     summMass = summMass + Number(mass[i]);
+    // }
+
+    do{
         enteredNumber = prompt('Введите целое число!', '');
-        while(enteredNumber == ' ' || enteredNumber == null || enteredNumber == '' || isNaN(enteredNumber) == true){
-            alert('Вы ничего не ввели или ввели не число, введите число');
-            enteredNumber = prompt('Введите целое число!', '');
+        console.log(enteredNumber);
+        if(enteredNumber == ' ' || enteredNumber == null || enteredNumber == '' || isNaN(enteredNumber) == true){
+            alert('Вы ничего не ввели или ввели не число');
+            break;
+        } else {
+            massStr = massStr + enteredNumber + ",";
+            //console.log(massStr);
         }
-        mass[i] = enteredNumber;
-        console.log(mass[i]);
+    }while(true);
+    
+    let mass = massStr.split(","); // превратили строчку в массив
+    for(let i=0; i<mass.length; i++){
         summMass = summMass + Number(mass[i]);
     }
+
     console.log("Сумма всех чисел массива:" + summMass);
     mass.sort(sortMass);
     console.log("Массив отсортирован:");
